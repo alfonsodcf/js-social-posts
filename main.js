71e0e39c6ac9bdb47522a79fb6a1a5a84a65da3d
alfonsodcf <alfonsoservizi01@gmail.com>
2022-04-19 10:10:32 +0200

    main:ponst-id

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ function printAllposts(){
                 </a>
             </div>
             <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                Piace a <b id="like-counter-${posts[i].id}" class="js-likes-counter">${posts[i].likes}</b> persone
             </div>
         </div> 
     </div>    
